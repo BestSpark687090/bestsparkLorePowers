4570ee5258f419dadb44178371c9c487b7c6c45d
Aelithron <aelithron@gmail.com>
2025-04-22 22:30:19 -0600

    Add Glitched Presence, begin adding Map Warp

--- src/main/java/com/nynsrulers/lorepowers/LorePowers.java
+++ src/main/java/com/nynsrulers/lorepowers/LorePowers.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityResurrectEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -55,6 +56,44 @@ public final class LorePowers extends JavaPlugin implements Listener {
             ((Player) lastCause).addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, 400, 2, true, true, true));
         }
     }
+
+    @EventHandler
+    public void onAttackGlitchedPresence(EntityDamageByEntityEvent e) {
+        if (e.isCancelled()) return;
+        if (e.getEntity() instanceof Player && checkPower(e.getEntity().getUniqueId(), Power.GLITCHED_PRESENCE)) {
+            Entity lastCause = e.getDamager();
+            if (lastCause instanceof Player) {
+                if (!((Player) lastCause).getInventory().getItemInMainHand().getType().toString().endsWith("_SWORD")) {
+                    lastCause.teleport(lastCause.getWorld().getHighestBlockAt(
+                            lastCause.getLocation().add(
+                                    (Math.random() - 0.5) * 200, 0, (Math.random() - 0.5) * 200
+                            ).getBlockX(),
+                            lastCause.getLocation().getBlockZ()
+                    ).getLocation().add(0.5, 1, 0.5));
+                    lastCause.sendMessage(CoreTools.getInstance().getPrefix() + ChatColor.RED + "You were glitched by " + e.getEntity().getName() + "'s presence!");
+                    e.getEntity().sendMessage(CoreTools.getInstance().getPrefix() + ChatColor.RED + "You glitched " + lastCause.getName() + "'s presence!");
+                } else {
+                    e.setDamage(e.getDamage() * 1.5);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onAttackMapWarp(EntityDamageByEntityEvent e) {
+        if (e.isCancelled()) return;
+        Entity lastCause = e.getDamager();
+        if (lastCause instanceof Player && checkPower(lastCause.getUniqueId(), Power.MAP_WARP)) {
+            Material weapon = ((Player) lastCause).getInventory().getItemInMainHand().getType();
+            if ((weapon.toString().endsWith("_SWORD") && e.getDamage() > 5) || // Stone sword damage is 5
+                    (weapon.toString().endsWith("_AXE") && e.getDamage() > 9)) { // Stone axe damage is 9
+                e.setCancelled(true);
+                lastCause.sendMessage(CoreTools.getInstance().getPrefix() + ChatColor.RED + "You cannot attack with this, as you are too weak!");
+            }
+        }
+    }
+    @EventHandler
+    public void onArmorEquipMapWarp() {}
 
     void reloadPlugin() {
         reloadConfig();
