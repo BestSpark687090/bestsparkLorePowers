0b54676387c3835e356be20361b29e7497fa839d
Nova <aelithron@gmail.com>
2025-09-26 20:33:21 -0600

    Quick velocity fix
    Signed-off-by: Nova <aelithron@gmail.com>

--- src/main/java/com/nynsrulers/lorepowers/LorePowers.java
+++ src/main/java/com/nynsrulers/lorepowers/LorePowers.java
@@ -349,7 +349,7 @@ public final class LorePowers extends JavaPlugin implements Listener {
         if (player.getInventory().getItemInMainHand().getType() != Material.AIR) return;
         if (cooldown.checkCooldown(player.getUniqueId(), Power.FOX_MAGIC)) return;
         cooldown.addCooldown(player.getUniqueId(), Power.FOX_MAGIC, 600L);
-        player.setVelocity(new Vector(0, 10, 0));
+        player.setVelocity(new Vector(player.getVelocity().getX(), 1.5, player.getVelocity().getZ()));
         player.sendMessage(CoreTools.getInstance().getPrefix() + ChatColor.GREEN + "You pounced on " + e.getEntity().getName() + ChatColor.GREEN + "!");
         getServer().getScheduler().runTaskLater(this, () -> {
             for (Entity entity : player.getWorld().getNearbyEntities(player.getLocation(), 2, 6, 2)) {
